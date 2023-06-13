b9a0abf99f6f07519c4f849e8e69ecb9434eb5b4
Valentina Zapotoczny <valen.czny@gmail.com>
2023-06-13 11:49:05 -0300

    avances hasta la clase 4

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true},
+  { text: 'Tomar el curso de intro a React', completed: false},
+  { text: 'Llorar', completed: false},
+  { text: 'LALALALALALA', completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
-
-      <TodoCounter />
+    <>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
       
       <CreateTodoButton />
-    </div>
+    </>
   );
 }
 
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,9 @@
-function TodoCounter() {
+import './TodoCounter.css';
+
+function TodoCounter({total, completed}) {
     return (
-      <h1>
-        Has completado 3 de 5 TODOS
+      <h1 >
+        Has completado {completed} de {total} TODOS
       </h1>
     )
   }
